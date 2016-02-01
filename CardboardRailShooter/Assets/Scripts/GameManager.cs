49512946bed869f9616eedd952765b9f83a7f4da
Ryan Stevenson <rystevenson93@googlemail.com>
2016-02-01 22:06:43 +0000

    Initial Project Commit And First Person Camera Movement Controller.

---
+++ CardboardRailShooter/Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	private static GameManager instance;
+    public static GameManager Instance
+    {
+        get{
+            return instance;
+        }
+        private set
+        {
+            if(instance != null)
+            {
+                Destroy(value);
+            }
+            else
+            {
+                instance = value;
+            }
+        }
+    }
+    
+    private InputManager inputManager;
+    public InputManager InputManager
+    {
+        get{return inputManager;}
+    }
+    private void Awake()
+    {
+        Instance = this;
+        Cursor.lockState = CursorLockMode.Locked;
+        inputManager = GetComponent<InputManager>();
+    }
+    
+    private void Update()
+    {
+        inputManager.Update();
+    }
+}
---
+++ CardboardRailShooter/Assets/Scripts/InputManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+
+    Vector3 temp = Vector3.zero;
+    Vector3 temp1 = Vector3.zero;
+    public void Update()
+    {
+        temp.x = -Input.GetAxis("Mouse Y");
+        temp.y = Input.GetAxis("Mouse X");
+        temp.z = 0f;
+        Camera.main.gameObject.transform.Rotate(temp * 100 * Time.deltaTime);
+        
+        temp1 = Camera.main.gameObject.transform.localEulerAngles;
+        temp1.z = 0;
+        Camera.main.gameObject.transform.localEulerAngles = temp1;
+    }
+}
